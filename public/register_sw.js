1f3c87eb621c5c78f51a4cd5238a6b9c599c0dfb
Muhammad Rasyidi <muhammadrasyidi17@gmail.com>
2018-11-24 13:52:47 +0700

    Edit manifest

--- public/register_sw.js
+++ public/register_sw.js
@@ -3,7 +3,15 @@ if(navigator.serviceWorker){
         console.log(`Service worker terdaftar, berada di ${reg.scope}`);
     }).catch(err => {
         console.log(`Service worker gagal di daftarkan, error: ${err}`);
-    })
+    });
+
+    navigator.serviceWorker.register(`/manifest.json`).then(reg => {
+        console.log(`Berhasil menambahkan manifest`);
+    }).catch(err => {
+        console.log(`Gagal menambahkan manifest`);
+    });
+
+
 }
 
 if(!navigator.onLine){
